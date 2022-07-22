530e2c08822628a08ff70e68f9d5408857eb7f16
Elozino A. Ovedhe <ovedhee@gmail.com>
2022-07-22 04:37:29 +0100

    cart functionality

---
+++ frontend-next/context/StateContext.js
@@ -0,0 +1,111 @@
+import React, { createContext, useContext, useState } from "react";
+import { toast } from "react-hot-toast";
+
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+  const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQuantities, setTotalQuantities] = useState(0);
+  const [qty, setQty] = useState(1);
+
+  let foundProduct;
+  let index;
+
+  const increaseQty = () => {
+    setQty((prevQty) => prevQty + 1);
+  };
+  const decreaseQty = () => {
+    setQty((prevQty) => {
+      if (prevQty - 1 < 1) return 1;
+      return prevQty - 1;
+    });
+  };
+
+  const onAdd = (product, quantity) => {
+    const checkProductInCart = cartItems.find(
+      (item) => item._id === product._id
+    );
+
+    setTotalPrice(
+      (prevTotalPrice) => prevTotalPrice + product.price * quantity
+    );
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+
+    if (checkProductInCart) {
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id)
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + quantity,
+          };
+      });
+
+      setCartItems(updatedCartItems);
+    } else {
+      product.quantity = quantity;
+
+      setCartItems([...cartItems, { ...product }]);
+    }
+
+    toast.success(`${qty} ${product.name} added to the cart.`);
+  };
+
+  const onRemove = (product) => {
+    foundProduct = cartItems.find((item) => item._id === product._id);
+    const newCartItem = cartItems.filter((item) => item._id !== product._id);
+
+    setTotalPrice((prev) => prev - foundProduct.price * foundProduct.quantity);
+    setTotalQuantities((prev) => prev - foundProduct.quantity);
+    setCartItems(newCartItem);
+  };
+
+  const toggleCartItemQuantity = (id, value) => {
+    foundProduct = cartItems.find((item) => item._id === id);
+    index = cartItems.find((product) => product._id === id);
+    const newCartItem = cartItems.filter((item) => item._id !== id);
+
+    if (value === "increment") {
+      setCartItems([
+        ...newCartItem,
+        { ...foundProduct, quantity: foundProduct.quantity + 1 },
+      ]);
+      setTotalPrice((prev) => prev + foundProduct.price);
+      setTotalQuantities((prev) => prev + 1);
+      // DO NOT MUTATE THE STATE DIRECTLY
+      // foundProduct.quantity += 1;
+      // cartItems[index] = foundProduct;
+    } else if (value == "decrement") {
+      if (foundProduct.quantity > 1) {
+        setCartItems([
+          ...newCartItem,
+          { ...foundProduct, quantity: foundProduct.quantity - 1 },
+        ]);
+        setTotalPrice((prev) => prev - foundProduct.price);
+        setTotalQuantities((prev) => prev - 1);
+      }
+    }
+  };
+
+  return (
+    <Context.Provider
+      value={{
+        showCart,
+        setShowCart,
+        cartItems,
+        totalPrice,
+        totalQuantities,
+        qty,
+        increaseQty,
+        decreaseQty,
+        onAdd,
+        toggleCartItemQuantity,
+        onRemove,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
+export const useStateContext = () => useContext(Context);
--- frontend-next/pages/product/[slug].js
+++ frontend-next/pages/product/[slug].js
@@ -1,46 +1,127 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiFillStar,
+  AiOutlineStar,
+} from "react-icons/ai";
 
 import { client, urlFor } from "../../libs/client";
+import { Product } from "../../components";
+import { useStateContext } from "../../context/StateContext";
 
 const ProductDetails = ({ product, products }) => {
-  const { name, image, details, price } = product;
+  // console.log({ product });
+  // console.log({ products });
+  const { image, name, details, price } = product;
+  const [index, setIndex] = useState(0);
+  const { increaseQty, decreaseQty, qty, onAdd, setShowCart } =
+    useStateContext();
+
+  // const handleBuyNow = () => {
+  //   onAdd(product, qty);
+
+  //   setShowCart(true);
+  // }
+
   return (
     <div>
-      <div className="product-details-container">
+      <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[0])} alt="image" />
+            <img
+              src={urlFor(image && image[index])}
+              className="product-detail-image"
+            />
           </div>
-          {/* <div className="small-images-container">
+          <div className="small-images-container">
             {image?.map((item, i) => (
-              <img src={urlFor(item)} alt="image" 
-              className=""
-              onMouseEnter={""}
+              <img
+                key={i}
+                src={urlFor(item)}
+                className={
+                  i === index ? "small-image selected-image" : "small-image"
+                }
+                onMouseEnter={() => setIndex(i)}
               />
             ))}
-          </div> */}
+          </div>
         </div>
-        <div className="product-details-desc">
+
+        <div className="product-detail-desc">
           <h1>{name}</h1>
-          <div></div>
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>(20)</p>
+          </div>
+          <h4>Details: </h4>
+          <p>{details}</p>
+          <p className="price">${price}</p>
+          <div className="quantity">
+            <h3>Quantity:</h3>
+            <p className="quantity-desc">
+              <span className="minus" onClick={decreaseQty}>
+                <AiOutlineMinus />
+              </span>
+              <span className="num">{qty}</span>
+              <span className="plus" onClick={increaseQty}>
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
+          <div className="buttons">
+            <button
+              type="button"
+              className="add-to-cart"
+              onClick={() => onAdd(product, qty)}
+            >
+              Add to Cart
+            </button>
+            <button
+              type="button"
+              className="buy-now"
+              // onClick={handleBuyNow}
+            >
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default ProductDetails;
-
 export const getStaticPaths = async () => {
   const query = `*[_type == "product"] {
     slug {
       current
     }
-  }`;
+  }
+  `;
 
   const products = await client.fetch(query);
+
   const paths = products.map((product) => ({
-    params: { slug: product.slug.current },
+    params: {
+      slug: product.slug.current,
+    },
   }));
 
   return {
@@ -50,13 +131,17 @@ export const getStaticPaths = async () => {
 };
 
 export const getStaticProps = async ({ params: { slug } }) => {
-  const query = `*[_type == 'product' && slug.current == "${slug}"][0]`;
-  const productsQuery = `*[_type == 'product']`;
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+  const productsQuery = '*[_type == "product"]';
 
   const product = await client.fetch(query);
   const products = await client.fetch(productsQuery);
 
+  // console.log(product);
+
   return {
-    props: { product, products },
+    props: { products, product },
   };
 };
+
+export default ProductDetails;
